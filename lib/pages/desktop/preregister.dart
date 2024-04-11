f743962d804c5caadb5bf314593939ce3064d47f
saddcow <jabiada@gbox.adnu.edu.ph>
2024-04-11 14:20:22 +0800

    updated late push

--- lib/pages/desktop/preregister.dart
+++ lib/pages/desktop/preregister.dart
@@ -29,85 +29,93 @@ class _PreRegisterPageState extends State<PreRegisterPage> {
 Widget _buildBox(context) {
   return Container(
     padding: EdgeInsets.symmetric(vertical: 180.0),
-    child: Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Container(
-          width: 840,
-          height: 800,
-          color: Color(0xFFF2F2F2),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(vertical: 160.0, horizontal: 120.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Certified Artificial Intelligence Professional',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 35,
-                    fontWeight: FontWeight.w600,
-                  ),
+    child: SizedBox(
+      width: 1690,
+      height: 800,
+      child: Card(
+        elevation: 2,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 840,
+              height: 800,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  bottomLeft: Radius.circular(20.0),
                 ),
-                SizedBox(height: 30),
-                Text(
-                  'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-                SizedBox(height: 40),
-                Text(
-                  'January 6 - 7, 2024',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w600
-                  ),
-                ),
-                SizedBox(height: 15),
-                Text(
-                  'Online Training',
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.w400
-                  ),
-                ),
-                SizedBox(height: 40),
-                Text(
-                  '₱ 5600',
-                  style: TextStyle(
-                    fontSize: 25,
-                    fontWeight: FontWeight.w600
-                  ),
-                ),
-                SizedBox(height: 40),
-                FilledButton(
-                  onPressed: () {}, 
-                  child: Text(
-                    'Pre-Register',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600
+                color: Color(0xFFF2F2F2),
+              ),
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 160.0, horizontal: 120.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Certified Artificial Intelligence Professional',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 35,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
-                  )
-                )
-              ],
+                    SizedBox(height: 30),
+                    Text(
+                      'Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                    SizedBox(height: 40),
+                    Text(
+                      'January 6 - 7, 2024',
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+                    ),
+                    SizedBox(height: 15),
+                    Text(
+                      'Online Training',
+                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.w400),
+                    ),
+                    SizedBox(height: 40),
+                    Text(
+                      '₱ 5600',
+                      style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+                    ),
+                    SizedBox(height: 40),
+                    FilledButton(
+                        onPressed: () {},
+                        child: Text(
+                          'Pre-Register',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600),
+                        ))
+                  ],
+                ),
+              ),
             ),
-          ),
+            Container(
+                width: 840,
+                height: 800,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(20.0),
+                    bottomRight: Radius.circular(20.0),
+                  ),
+                  child: Image.asset(
+                    'assets/images/course1.png',
+                    fit: BoxFit.fill,
+                  ),
+                ))
+          ],
         ),
-        Container(
-          width: 840,
-          height: 800,
-          child: Image.asset(
-            'assets/images/course1.png',
-            fit: BoxFit.fill,
-          ),
-        )
-      ],
+      ),
     ),
   );
 }
